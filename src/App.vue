dcbd15dfaed4785937ba4219c57735f10cf2d2bc
이지훈 <dlwlgns102@naver.com>
2021-06-29 14:29:41 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to 이지훈's Vue.js App" />
 </template>
 
 <script>
